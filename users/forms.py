15115d12aa733ce8479a6e6de640be4b787fa34d
mahdibagheri <bagheri.almas@gmail.com>
2023-03-15 21:15:41 +0330

    decrease register fields for registration faster

--- users/forms.py
+++ users/forms.py
@@ -5,15 +5,15 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'username','password1','password2', 'email', 'facebook', 'twitter', 'avatar', 'about_me']
+        fields = ['username', 'password1', 'password2', 'email', 'avatar']
         # fields = ['username', 'password1','password2', 'avatar',]
 
 
 class CustomUserChangeForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'username','password1','password2', 'email', 'facebook', 'twitter', 'avatar', 'about_me']
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2', 'email', 'facebook', 'twitter',
+                  'avatar', 'about_me']
 
         # fields = ['first_name', 'last_name', 'username', 'email', 'instagram', 'twitter', 'avatar', 'about_me']
         # fields = ['username', 'password1','password2', 'avatar',]
-
