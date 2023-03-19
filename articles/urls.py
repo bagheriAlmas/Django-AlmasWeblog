aed495043d6eb71d5d8d9d3d80a95f14bfe918ae
mahdibagheri <bagheri.almas@gmail.com>
2023-03-19 13:32:13 +0330

    add viewSet url with router

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from articles.views.template_views import article_list_view, article_detail_view
+from .views.api_views import ArticleView
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'', ArticleView)
 
 urlpatterns = [
     path('', article_list_view, name='article_list'),
     path('articles/<slug:slug>/', article_detail_view, name='article_detail'),
+    path('api/articles/', include(router.urls))
 ]
